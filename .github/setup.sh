15606bd988144c93686850222314c5e5872f0afe
Jean-Yves Tinevez <tinevez@pasteur.fr>
2021-07-02 12:37:15 +0200

    CI: add executable script .github/setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/github-action-ci.sh
-sh github-action-ci.sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-setup-github-actions.sh
+sh ci-setup-github-actions.sh
