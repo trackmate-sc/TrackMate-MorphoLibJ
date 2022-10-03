ae4e9d19485577a507be995d2025e467d45ae98d
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-10-03 17:13:30 +0200

    Use the new detection preview and info text facilities.
    And remove a remaining but unused PrefService instance.

--- src/main/java/fiji/plugin/trackmate/morpholibj/MorphoLibJDetectorConfigurationPanel.java
+++ src/main/java/fiji/plugin/trackmate/morpholibj/MorphoLibJDetectorConfigurationPanel.java
@@ -24,9 +24,7 @@ package fiji.plugin.trackmate.morpholibj;
 import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
 import static fiji.plugin.trackmate.detection.ThresholdDetectorFactory.KEY_SIMPLIFY_CONTOURS;
 import static fiji.plugin.trackmate.gui.Fonts.BIG_FONT;
-import static fiji.plugin.trackmate.gui.Fonts.FONT;
 import static fiji.plugin.trackmate.gui.Fonts.SMALL_FONT;
-import static fiji.plugin.trackmate.gui.Icons.PREVIEW_ICON;
 import static fiji.plugin.trackmate.morpholibj.MorphoLibJDetectorFactory.KEY_CONNECTIVITY;
 import static fiji.plugin.trackmate.morpholibj.MorphoLibJDetectorFactory.KEY_TOLERANCE;
 
@@ -44,7 +42,6 @@ import java.util.Map;
 import java.util.Vector;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
@@ -52,16 +49,13 @@ import javax.swing.JLabel;
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 
-import org.scijava.prefs.PrefService;
-
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.detection.DetectionUtils;
 import fiji.plugin.trackmate.detection.SpotDetectorFactoryBase;
 import fiji.plugin.trackmate.gui.GuiUtils;
 import fiji.plugin.trackmate.gui.components.ConfigurationPanel;
-import fiji.plugin.trackmate.util.JLabelLogger;
-import fiji.plugin.trackmate.util.TMUtils;
+import fiji.plugin.trackmate.util.DetectionPreview;
 
 public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 {
@@ -78,32 +72,18 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 
 	private final JFormattedTextField ftfTolerance;
 
-	protected final PrefService prefService;
-
 	private final JCheckBox chkboxSimplify;
 
 	private final JComboBox< Connectivity > cmbboxConnectivity;
 
 	public MorphoLibJDetectorConfigurationPanel( final Settings settings, final Model model )
 	{
-		this.prefService = TMUtils.getContext().getService( PrefService.class );
-
 		final GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 144, 0, 32 };
-		gridBagLayout.rowHeights = new int[] { 0, 0, 84, 0, 27, 0, 0, 0, 0, 37, 23 };
+		gridBagLayout.rowHeights = new int[] { 0, 84, 0, 27, 0, 0, 0 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0 };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 };
+		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
 		setLayout( gridBagLayout );
-
-		final JLabel lblSettingsForDetector = new JLabel( "Settings for detector:" );
-		lblSettingsForDetector.setFont( FONT );
-		final GridBagConstraints gbcLblSettingsForDetector = new GridBagConstraints();
-		gbcLblSettingsForDetector.gridwidth = 3;
-		gbcLblSettingsForDetector.insets = new Insets( 5, 5, 5, 0 );
-		gbcLblSettingsForDetector.fill = GridBagConstraints.HORIZONTAL;
-		gbcLblSettingsForDetector.gridx = 0;
-		gbcLblSettingsForDetector.gridy = 0;
-		add( lblSettingsForDetector, gbcLblSettingsForDetector );
 
 		final JLabel lblDetector = new JLabel( TITLE, ICON, JLabel.RIGHT );
 		lblDetector.setFont( BIG_FONT );
@@ -113,25 +93,21 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		gbcLblDetector.insets = new Insets( 5, 5, 5, 0 );
 		gbcLblDetector.fill = GridBagConstraints.HORIZONTAL;
 		gbcLblDetector.gridx = 0;
-		gbcLblDetector.gridy = 1;
+		gbcLblDetector.gridy = 0;
 		add( lblDetector, gbcLblDetector );
 
 		/*
 		 * Help text.
 		 */
-		final JLabel lblHelptext = new JLabel( MorphoLibJDetectorFactory.INFO_TEXT
-				.replace( "<br>", "" )
-				.replace( "<p>", "<p align=\"justify\">" )
-				.replace( "<html>", "<html><p align=\"justify\">" ) );
-		lblHelptext.setFont( FONT.deriveFont( Font.ITALIC ) );
+
 		final GridBagConstraints gbcLblHelptext = new GridBagConstraints();
 		gbcLblHelptext.anchor = GridBagConstraints.NORTH;
-		gbcLblHelptext.fill = GridBagConstraints.HORIZONTAL;
+		gbcLblHelptext.fill = GridBagConstraints.BOTH;
 		gbcLblHelptext.gridwidth = 3;
-		gbcLblHelptext.insets = new Insets( 5, 10, 5, 10 );
+		gbcLblHelptext.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblHelptext.gridx = 0;
-		gbcLblHelptext.gridy = 2;
-		add( lblHelptext, gbcLblHelptext );
+		gbcLblHelptext.gridy = 1;
+		add( GuiUtils.textInScrollPanel( GuiUtils.infoDisplay( MorphoLibJDetectorFactory.INFO_TEXT ) ), gbcLblHelptext );
 
 		/*
 		 * Channel selector.
@@ -143,7 +119,7 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		gbcLblSegmentInChannel.anchor = GridBagConstraints.EAST;
 		gbcLblSegmentInChannel.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblSegmentInChannel.gridx = 0;
-		gbcLblSegmentInChannel.gridy = 3;
+		gbcLblSegmentInChannel.gridy = 2;
 		add( lblSegmentInChannel, gbcLblSegmentInChannel );
 
 		sliderChannel = new JSlider();
@@ -151,7 +127,7 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		gbcSliderChannel.fill = GridBagConstraints.HORIZONTAL;
 		gbcSliderChannel.insets = new Insets( 5, 5, 5, 5 );
 		gbcSliderChannel.gridx = 1;
-		gbcSliderChannel.gridy = 3;
+		gbcSliderChannel.gridy = 2;
 		add( sliderChannel, gbcSliderChannel );
 
 		final JLabel labelChannel = new JLabel( "1" );
@@ -160,7 +136,7 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		final GridBagConstraints gbcLabelChannel = new GridBagConstraints();
 		gbcLabelChannel.insets = new Insets( 5, 5, 5, 0 );
 		gbcLabelChannel.gridx = 2;
-		gbcLabelChannel.gridy = 3;
+		gbcLabelChannel.gridy = 2;
 		add( labelChannel, gbcLabelChannel );
 
 		sliderChannel.addChangeListener( l -> labelChannel.setText( "" + sliderChannel.getValue() ) );
@@ -175,7 +151,7 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		gbcLblTolerance.anchor = GridBagConstraints.EAST;
 		gbcLblTolerance.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblTolerance.gridx = 0;
-		gbcLblTolerance.gridy = 4;
+		gbcLblTolerance.gridy = 3;
 		add( lblTolerance, gbcLblTolerance );
 
 		ftfTolerance = new JFormattedTextField( THRESHOLD_FORMAT );
@@ -185,9 +161,9 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		final GridBagConstraints gbcTolerance = new GridBagConstraints();
 		gbcTolerance.gridwidth = 2;
 		gbcTolerance.fill = GridBagConstraints.HORIZONTAL;
-		gbcTolerance.insets = new Insets( 5, 5, 5, 5 );
+		gbcTolerance.insets = new Insets( 5, 5, 5, 0 );
 		gbcTolerance.gridx = 1;
-		gbcTolerance.gridy = 4;
+		gbcTolerance.gridy = 3;
 		add( ftfTolerance, gbcTolerance );
 
 		/*
@@ -198,9 +174,9 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		lblConnectivity.setFont( new Font( "Arial", Font.PLAIN, 10 ) );
 		final GridBagConstraints gbcLblConnectivity = new GridBagConstraints();
 		gbcLblConnectivity.anchor = GridBagConstraints.EAST;
-		gbcLblConnectivity.insets = new Insets( 0, 0, 5, 5 );
+		gbcLblConnectivity.insets = new Insets( 0, 5, 5, 5 );
 		gbcLblConnectivity.gridx = 0;
-		gbcLblConnectivity.gridy = 5;
+		gbcLblConnectivity.gridy = 4;
 		add( lblConnectivity, gbcLblConnectivity );
 
 		this.cmbboxConnectivity = new JComboBox<>( new Vector<>( Arrays.asList( Connectivity.values() ) ) );
@@ -208,10 +184,10 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		cmbboxConnectivity.setFont( SMALL_FONT );
 		final GridBagConstraints gbcCmbboxConnectivity = new GridBagConstraints();
 		gbcCmbboxConnectivity.gridwidth = 2;
-		gbcCmbboxConnectivity.insets = new Insets( 0, 0, 5, 5 );
+		gbcCmbboxConnectivity.insets = new Insets( 0, 5, 5, 0 );
 		gbcCmbboxConnectivity.fill = GridBagConstraints.HORIZONTAL;
 		gbcCmbboxConnectivity.gridx = 1;
-		gbcCmbboxConnectivity.gridy = 5;
+		gbcCmbboxConnectivity.gridy = 4;
 		add( cmbboxConnectivity, gbcCmbboxConnectivity );
 
 		/*
@@ -222,57 +198,45 @@ public class MorphoLibJDetectorConfigurationPanel extends ConfigurationPanel
 		lblSimplify.setFont( SMALL_FONT );
 		final GridBagConstraints gbcLblSimplify = new GridBagConstraints();
 		gbcLblSimplify.anchor = GridBagConstraints.EAST;
-		gbcLblSimplify.insets = new Insets( 0, 0, 5, 5 );
+		gbcLblSimplify.insets = new Insets( 0, 5, 5, 5 );
 		gbcLblSimplify.gridx = 0;
-		gbcLblSimplify.gridy = 6;
+		gbcLblSimplify.gridy = 5;
 		add( lblSimplify, gbcLblSimplify );
 
 		this.chkboxSimplify = new JCheckBox();
 		final GridBagConstraints gbcChkboxSimplify = new GridBagConstraints();
 		gbcChkboxSimplify.anchor = GridBagConstraints.WEST;
-		gbcChkboxSimplify.insets = new Insets( 0, 0, 5, 5 );
+		gbcChkboxSimplify.insets = new Insets( 0, 5, 5, 5 );
 		gbcChkboxSimplify.gridx = 1;
-		gbcChkboxSimplify.gridy = 6;
+		gbcChkboxSimplify.gridy = 5;
 		add( chkboxSimplify, gbcChkboxSimplify );
 
 		/*
 		 * Logger.
 		 */
 
-		final JLabelLogger labelLogger = new JLabelLogger();
-		final GridBagConstraints gbcLabelLogger = new GridBagConstraints();
-		gbcLabelLogger.gridwidth = 3;
-		gbcLabelLogger.gridx = 0;
-		gbcLabelLogger.gridy = 10;
-		add( labelLogger, gbcLabelLogger );
-
-		/*
-		 * Preview.
-		 */
-
-		final JButton btnPreview = new JButton( "Preview", PREVIEW_ICON );
-		btnPreview.setFont( FONT );
 		final GridBagConstraints gbcBtnPreview = new GridBagConstraints();
-		gbcBtnPreview.gridwidth = 1;
-		gbcBtnPreview.anchor = GridBagConstraints.SOUTHEAST;
-		gbcBtnPreview.insets = new Insets( 5, 5, 5, 0 );
-		gbcBtnPreview.gridx = 2;
-		gbcBtnPreview.gridy = 9;
-		add( btnPreview, gbcBtnPreview );
+		gbcBtnPreview.fill = GridBagConstraints.HORIZONTAL;
+		gbcBtnPreview.gridwidth = 3;
+		gbcBtnPreview.fill = GridBagConstraints.BOTH;
+		gbcBtnPreview.insets = new Insets( 5, 5, 5, 5 );
+		gbcBtnPreview.gridx = 0;
+		gbcBtnPreview.gridy = 6;
+
+		final DetectionPreview detectionPreview = DetectionPreview.create()
+				.model( model )
+				.settings( settings )
+				.detectorFactory( getDetectorFactory() )
+				.detectionSettingsSupplier( () -> getSettings() )
+				.axisLabel( DetectionUtils.is2D( settings.imp ) ? "Area histogram" : "Volume histogram" )
+				.get();
+		add( detectionPreview.getPanel(), gbcBtnPreview );
 
 		/*
 		 * Listeners and specificities.
 		 */
 
 		GuiUtils.selectAllOnFocus( ftfTolerance );
-		btnPreview.addActionListener( e -> DetectionUtils.preview(
-				model,
-				settings,
-				getDetectorFactory(),
-				getSettings(),
-				settings.imp.getFrame() - 1,
-				labelLogger.getLogger(),
-				b -> btnPreview.setEnabled( b ) ) );
 
 		/*
 		 * Deal with channels: the slider and channel labels are only visible if
